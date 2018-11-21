4ab3b752ae87f0367e1b21826ae92cddbf34dc61
NinaTanneberger <nina.tanneberger@gmail.com>
2018-11-22 08:06:28 +1000

    initial install

--- newInstall_v2/extensions/Wikibase/repo/tests/selenium/LocalSettings.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-$wgEnableWikibaseRepo = true;
-$wgEnableWikibaseClient = false;
-require_once "$IP/extensions/Wikibase/repo/Wikibase.php";
-require_once "$IP/extensions/Wikibase/repo/ExampleSettings.php";
--- newInstall_v2/extensions/Wikibase/view/resources/wikibase/entityIdFormatter/EntityIdHtmlFormatter.js
+++
@@ -1,9 +0,0 @@
-( function ( wb ) {
-	'use strict';
-
-	var SELF = wb.entityIdFormatter.EntityIdHtmlFormatter = function () {
-	};
-
-	SELF.prototype.format = util.abstractMember;
-
-}( wikibase ) );
