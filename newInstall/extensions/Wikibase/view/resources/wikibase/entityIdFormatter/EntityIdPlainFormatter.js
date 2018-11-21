c94c80ba69a23eba937045d812dc73c27beada80
NinaTanneberger <nina.tanneberger@gmail.com>
2018-11-21 21:33:46 +1000

    delete files

--- newInstall/extensions/Wikibase/view/resources/wikibase/entityIdFormatter/EntityIdPlainFormatter.js
+++
@@ -1,9 +0,0 @@
-( function ( wb ) {
-	'use strict';
-
-	var SELF = wb.entityIdFormatter.EntityIdPlainFormatter = function () {
-	};
-
-	SELF.prototype.format = util.abstractMember;
-
-}( wikibase ) );
--- rockartdatabase_v3/newInstall/extensions/Capiunto/tests/phpunit/output/BasicRowTest.lua
+++
@@ -1,9 +0,0 @@
-local capiunto = require 'capiunto'
-local box = capiunto.create( {
-	title = 'Infobox Data Test!'
-} )
-box
-	:addHeader( 'A header' )
-	:addRow( 'An item', 'with a value' )
-
-return tostring( box:getHtml() )
