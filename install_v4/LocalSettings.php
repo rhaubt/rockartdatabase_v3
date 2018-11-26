26cad270420da29017ccd71a78af6f17611daa1c
NinaTanneberger <nina.tanneberger@gmail.com>
2018-11-26 18:13:05 +1000

    update with logo

--- install_v4/LocalSettings.php
+++ install_v4/LocalSettings.php
@@ -37,11 +37,8 @@ $wgResourceBasePath = $wgScriptPath;
 
 ## The URL path to the logo.  Make sure you change this from the default,
 ## or else you'll overwrite your logo when you upgrade!
-$wgLogo = "$wgResourceBasePath/resources/assets/wiki.png";
+$wgLogo = "$wgResourceBasePath/resources/assets/thumbLogo.png";
 
-## The URL path to the logo.  Make sure you change this from the default,
-## or else you'll overwrite your logo when you upgrade!
-$wgLogo = "$wgResourceBasePath/resources/assets/wiki.png"; // does this need to be here?
 
 ## UPO means: this is also a user preference option
 
